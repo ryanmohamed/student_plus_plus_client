d021cf7fd04fcaf6906c74bf34afbea002b0f645
Yasharel Saiedian <yasharelsdn@gmail.com>
2022-05-12 01:28:58 -0400

    Implement Assignment form and add more side panels

--- src/pages/SidePanel.js
+++ src/pages/SidePanel.js
@@ -7,17 +7,69 @@ function SidePanel({passPanel}){
 
     const [assignmentButton, setAssignmentButton] = useState(true);
     const [coursesButton, setCoursesButton] = useState(false);
+    const [priorityListButton, setPriorityListButton] = useState(false);
+    const [goalsButton, setGoalsButton] = useState(false);
+    const [settingsButton, setSettingsButton] = useState(false);
+    const [tagsButton, setTagsButton] = useState(false);
 
     const handleClick1 = () => {
         setAssignmentButton(true);
         setCoursesButton(false);
+        setPriorityListButton(false);
+        setGoalsButton(false);
+        setSettingsButton(false);
+        setTagsButton(false);
         passPanel(1);
     }
 
     const handleClick2 = () => {
         setCoursesButton(true);
         setAssignmentButton(false);
+        setPriorityListButton(false);
+        setGoalsButton(false);
+        setSettingsButton(false);
+        setTagsButton(false);
         passPanel(2);
+    }
+
+    const handleClick3 = () => {
+        setPriorityListButton(true);
+        setCoursesButton(false);
+        setAssignmentButton(false);
+        setGoalsButton(false);
+        setSettingsButton(false);
+        setTagsButton(false);
+        passPanel(3);
+    }
+
+    const handleClick4 = () => {
+        setGoalsButton(true);
+        setCoursesButton(false);
+        setAssignmentButton(false);
+        setPriorityListButton(false);
+        setSettingsButton(false);
+        setTagsButton(false);
+        passPanel(4);
+    }
+
+    const handleClick5 = () => {
+        setSettingsButton(true);
+        setAssignmentButton(false);
+        setCoursesButton(false);
+        setPriorityListButton(false);
+        setGoalsButton(false);
+        setTagsButton(false);
+        passPanel(5);
+    }
+
+    const handleClick6 = () => {
+        setTagsButton(true);
+        setAssignmentButton(false);
+        setCoursesButton(false);
+        setPriorityListButton(false);
+        setGoalsButton(false);
+        setSettingsButton(false);
+        passPanel(6);
     }
 
     return (
@@ -26,6 +78,11 @@ function SidePanel({passPanel}){
             <ul>
                 <li className={assignmentButton && "active"} onClick={handleClick1}>📓</li>
                 <li className={coursesButton && "active"} onClick={handleClick2}>👨‍🏫</li>
+                <li className={priorityListButton && "active"} onClick={handleClick3}>Placeholder</li>
+                <li className={goalsButton && "active"} onClick={handleClick4}>Placeholder</li>
+                <li className={settingsButton && "active"} onClick={handleClick5}>Placeholder</li>
+                <li className={tagsButton && "active"} onClick={handleClicks6}>Placeholder</li>
+
             </ul>
 
         </div>
