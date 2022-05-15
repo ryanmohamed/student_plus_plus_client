6648c243486abc86d493ffb4fed39829d7499254
Yasharel Saiedian <yasharelsdn@gmail.com>
2022-05-15 16:09:01 -0400

    Add assignment update form

--- src/pages/Assignment.js
+++ src/pages/Assignment.js
@@ -1,6 +1,7 @@
 import './Assignments.css';
 import react from 'react';
 import { useState } from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 
 function Assignment({courseName, name, dueDate, weight, priority}){
     
@@ -9,13 +10,43 @@ function Assignment({courseName, name, dueDate, weight, priority}){
     const off = () => { setActive(false) };
     const on = () => { setActive(true) }
 
+    const [formActive, setFormActive] = useState(false);
+
+    const toggleForm = () => { setFormActive(!formActive) };
+
+    const initialValues = {
+      estimatedCompletionTime: "",
+      scoreReceived: ""
+    };
+    
     return (
         <div className={active ? "assignment touched-assignment" : "assignment"} onMouseOver={on} onMouseLeave={off}>
             <span class="course-name">{courseName}</span>
             <span class="name">{name}</span>
             <span class="dueDate">{dueDate}</span>
             <span class="weight">?/{weight}</span>
-            {active && <button>Update</button>}
+            {active && <button onClick={toggleForm}>Update</button>}
+            {formActive && <Formik initialValues={initialValues}>
+              <Form>
+                <label>Score received: </label> 
+                <ErrorMessage name="scoreReceived" component="span"/>
+                <Field 
+                        id="scoreReceived"
+                        name="scoreReceived" 
+                        type="text"
+                        placeholder="(Ex: 100)"
+                />
+
+                <label>Estimated Completion Time: </label> 
+                <ErrorMessage name="estimatedCompletionTime" component="span"/>
+                <Field 
+                        id="estimatedCompletionTime"
+                        name="estimatedCompletionTime" 
+                        type="text"
+                        placeholder="(Ex: 2 hours)"
+                />
+              </Form>
+            </Formik>}
         </div>
       );
 }
