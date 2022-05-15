613521d2f0d57618a2b28d6a202c6b7460dddc01
ryan <ryan@ryans-air.myfiosgateway.com>
2022-05-15 15:02:03 -0400

    added update button

--- src/pages/Assignment.js
+++ src/pages/Assignment.js
@@ -3,12 +3,19 @@ import react from 'react';
 import { useState } from 'react';
 
 function Assignment({courseName, name, dueDate, weight, priority}){
+    
+    const [active, setActive] = useState(false);
+
+    const off = () => { setActive(false) };
+    const on = () => { setActive(true) }
+
     return (
-        <div className="assignment">
+        <div className={active ? "assignment touched-assignment" : "assignment"} onMouseOver={on} onMouseLeave={off}>
             <span class="course-name">{courseName}</span>
             <span class="name">{name}</span>
             <span class="dueDate">{dueDate}</span>
             <span class="weight">?/{weight}</span>
+            {active && <button>Update</button>}
         </div>
       );
 }
